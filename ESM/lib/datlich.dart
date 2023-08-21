192a9f11cf0b2137dc1559d9ef78a90afde03a90
Lee An <19dh110830@st.huflit.edu.vn>
2023-08-21 10:23:16 +0700

    first commit

---
+++ ESM/lib/datlich.dart
@@ -0,0 +1,44 @@
+import 'package:esm/pages/home.dart';
+import 'package:esm/pages/online.dart';
+import 'package:flutter/material.dart';
+
+class DatLich extends StatefulWidget {
+  const DatLich({super.key});
+
+  @override
+  State<DatLich> createState() => _DatLichState();
+}
+
+class _DatLichState extends State<DatLich> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: screenHeight * 0.05,
+            backgroundColor: const Color(0xff4CB848),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Đặt lịch khám online'),
+                Tab(text: 'Đặt lịch khám tại nhà'),
+              ],
+            ),
+            title: const Text('Đặt lịch'),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_back),
+            ),
+          ),
+          body: const TabBarView(
+            children: [Online(), Home()],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ ESM/lib/pages/online.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Online extends StatefulWidget {
+  const Online({super.key});
+
+  @override
+  State<Online> createState() => _OnlineState();
+}
+
+class _OnlineState extends State<Online> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(),
+        ],
+      ),
+    );
+  }
+}
