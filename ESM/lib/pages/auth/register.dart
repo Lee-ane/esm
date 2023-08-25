b49cb9366495c3b8523e9a037bcbdb0e4b214feb
Lee An <19dh110830@st.huflit.edu.vn>
2023-08-25 11:38:38 +0700

    first commit

--- ESM/lib/pages/auth/register.dart
+++ ESM/lib/pages/auth/register.dart
@@ -1,12 +1,7 @@
-// ignore_for_file: use_build_context_synchronously
-
-import 'dart:convert';
 import 'package:esm/components/buttons.dart';
 import 'package:esm/model/data.dart';
 import 'package:esm/model/models.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
